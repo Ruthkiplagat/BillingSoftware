7087a49fbe8d2b88e9880357a06f624c2fa888e6
Ruth <ruthkiplagat254@gmail.com>
2021-03-27 23:12:07 +0300

    addd about page

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ def index():
 
     return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
